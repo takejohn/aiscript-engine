8a3e07b653d1d5f9dd38723ea4e49edfa66b6773
takejohn <takejohn@takejohn.jp>
2024-10-28 23:44:26 +0900

    未使用のフィールドや変数を削除

--- aiscript-engine-interpreter/src/interpreter.rs
+++ aiscript-engine-interpreter/src/interpreter.rs
@@ -5,25 +5,18 @@ use aiscript_engine_common::Result;
 use aiscript_engine_ir::translate;
 use aiscript_engine_vm::{Value, Vm, VmState};
 
-const IRQ_RATE: usize = 300;
-const IRQ_AT: usize = IRQ_RATE - 1;
-
-pub struct Interpreter {
-    step_count: usize,
-    // stop: bool,
-    // scope: Scope<'gc>
-}
+pub struct Interpreter;
 
 impl Interpreter {
     pub fn new() -> Self {
-        Interpreter { step_count: 0 }
+        Interpreter
     }
 
-    fn run(&mut self, program: &[ast::Node]) -> Result<Value> {
+    pub fn run(&mut self, program: &[ast::Node]) -> Result<Value> {
         let ir = translate(program);
         let mut vm = Vm::new(ir);
         while let VmState::Continue = vm.step()? {
-            self.step_count += 1;
+            // nop
         }
         return Ok(Value::Null);
     }
