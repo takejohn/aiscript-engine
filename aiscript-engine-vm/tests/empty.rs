9521ddef5203a35480a2cb07cbde79947e076465
takejohn <takejohn@takejohn.jp>
2024-11-09 20:34:28 +0900

    一部のテストの削除

--- aiscript-engine-vm/tests/empty.rs
+++
@@ -1,9 +0,0 @@
-use aiscript_engine_ir::Ir;
-use aiscript_engine_vm::Vm;
-
-#[test]
-fn empty() {
-    let mut ir = Ir::default();
-    let mut vm = Vm::new(&mut ir);
-    assert!(vm.exec().is_ok());
-}
